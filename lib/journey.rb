7f40a514487d99e3fd93bb938a5876bd4c48158a
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-09 15:48:37 +0100

    Created the methods for #start and #finish and passed tests

--- lib/journey.rb
+++ lib/journey.rb
@@ -5,4 +5,13 @@ class Journey
     @entry_station = nil
     @exit_station = nil
   end 
+
+  def start(station)
+    @entry_station = station
+  end
+
+  def finish(station)
+    @exit_station = station
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,11 +2,26 @@ require 'journey'
 
 describe Journey do
   subject(:journey) { Journey.new }
+
   it 'is created with an entry station' do
     expect(journey.entry_station).to eq nil
   end
 
   it 'is created with an exit station' do
     expect(journey.exit_station).to eq nil
+  end
+
+  it { is_expected.to respond_to(:start).with(1).argument }
+
+  it { is_expected.to respond_to(:finish).with(1).argument }
+
+  it 'changes entrance station to station input' do
+    journey.start('bank')
+    expect(journey.entry_station).to eq('bank')
+  end
+
+  it 'changes exit station to station input' do
+    journey.finish('fulham')
+    expect(journey.exit_station).to eq('fulham')
   end
 end
