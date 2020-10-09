f4830e5b3042bdcfb78ea930de27cb633801fd15
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-09 11:42:10 +0100

    Refactored oystercard_spec tests so subject now is called card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,79 +2,80 @@ require 'oystercard'
 
 describe Oystercard do
   let(:station) { double :station }
+  subject(:card) { Oystercard.new }
 
   it "initialized cards should have a balance of 0" do
-    expect(subject.balance).to eq 0
+    expect(card.balance).to eq 0
   end
 
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
   it 'can top up the balance' do
-    subject.top_up(10)
-    expect(subject.balance).to eq(10)
+    card.top_up(10)
+    expect(card.balance).to eq(10)
   end
 
   it 'raises error if the max-balance is exceeded' do 
-    subject.top_up(90)
-    expect { subject.top_up 1}.to raise_error "Max balance of #{Oystercard::DEFAULT_MAX} is exceeded"
+    card.top_up(90)
+    expect { card.top_up 1}.to raise_error "Max balance of #{Oystercard::DEFAULT_MAX} is exceeded"
   end 
 
   it 'is not in transit' do
-    expect(subject.in_journey?).to eq(false)
+    expect(card.in_journey?).to eq(false)
   end
 
   it 'it activates the card when touched in' do
-    subject.top_up(10)
-    subject.touch_in(:station)
-    expect(subject.in_journey?).to eq(true)
+    card.top_up(10)
+    card.touch_in(:station)
+    expect(card.in_journey?).to eq(true)
   end
 
   it 'it deactivates the card when touched out' do 
-    subject.top_up(10)
-    subject.touch_in(:station)
-    subject.touch_out(:station)
-    expect(subject.in_journey?).to eq(false)
+    card.top_up(10)
+    card.touch_in(:station)
+    card.touch_out(:station)
+    expect(card.in_journey?).to eq(false)
   end
 
   it 'would raise error if balance is below minimum fare' do 
-    expect { subject.touch_in(:station) }.to raise_error "Insufficient funds: Balance less than #{Oystercard::MINIMUM_FARE}"
+    expect { card.touch_in(:station) }.to raise_error "Insufficient funds: Balance less than #{Oystercard::MINIMUM_FARE}"
   end
 
   it 'deducts minimum fare when touching out' do
-    subject.top_up(10)
-    subject.touch_in(:station)
-    expect { subject.touch_out(:station) }.to change { subject.balance }.by -Oystercard::MINIMUM_FARE
+    card.top_up(10)
+    card.touch_in(:station)
+    expect { card.touch_out(:station) }.to change { card.balance }.by -Oystercard::MINIMUM_FARE
   end
 
   it 'knows the entry station' do
-    subject.top_up(10)
+    card.top_up(10)
 
-    expect { subject.touch_in(:station)}.to change { subject.entry_station}.to eq :station
+    expect { card.touch_in(:station)}.to change { card.entry_station}.to eq :station
   
   end
 
   it 'updates entry station to nil on touch out' do 
-    subject.top_up(10)
-    subject.touch_in(:station)
-    expect { subject.touch_out(:station) }.to change { subject.entry_station}.to eq nil
+    card.top_up(10)
+    card.touch_in(:station)
+    expect { card.touch_out(:station) }.to change { card.entry_station}.to eq nil
   end
 
   it 'has a journey history' do
-    expect(subject.journey_history).to eq []
+    expect(card.journey_history).to eq []
   end
 
   it 'knows the exit station' do
-    subject.top_up(10)
-    subject.touch_in(:station)
-    subject.touch_out(:station)
-    expect(subject.exit_station).to eq :station
+    card.top_up(10)
+    card.touch_in(:station)
+    card.touch_out(:station)
+    expect(card.exit_station).to eq :station
   end
 
   it 'stores one journey as a hash' do
-    subject.top_up(10)
-    subject.touch_in('Bank')
-    subject.touch_out('Fulham')
-    expect(subject.journey_history).to eq [{entry_station: 'Bank', exit_station: 'Fulham'}]
+    card.top_up(10)
+    card.touch_in('Bank')
+    card.touch_out('Fulham')
+    expect(card.journey_history).to eq [{entry_station: 'Bank', exit_station: 'Fulham'}]
   end
 
 end 
